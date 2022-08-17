1a770fd40642abebcbec144323b3c071aa36cafb
SagarMakhar <sagarmakhar@gmail.com>
2022-08-17 13:21:15 +0000

    BiometricScheduler: Cancel operation if not idle - some hals fail to report success/failure (for ex. realme fp hals)
    [DarkJoker360 - Switch to overlays]
    [timjosten - Adapt to 12.1.0_r8 merge changes]
    [ghostrider-reborn - Simplify code]
    
    Change-Id: I442ce063280af36a04c25fcbc3dd45a90f196988
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/core/java/com/android/server/biometrics/sensors/face/hidl/Face10.java
+++ services/core/java/com/android/server/biometrics/sensors/face/hidl/Face10.java
@@ -369,8 +369,8 @@ public class Face10 implements IHwBinder.DeathRecipient, ServiceProvider {
             @NonNull FaceSensorPropertiesInternal sensorProps,
             @NonNull LockoutResetDispatcher lockoutResetDispatcher) {
         final Handler handler = new Handler(Looper.getMainLooper());
-        return new Face10(context, sensorProps, lockoutResetDispatcher, handler,
-                new BiometricScheduler(TAG, BiometricScheduler.SENSOR_TYPE_FACE,
+        return new Face10(context, sensorProps, lockoutResetDispatcher, handler, 
+				new BiometricScheduler(context, TAG, BiometricScheduler.SENSOR_TYPE_FACE,
                         null /* gestureAvailabilityTracker */),
                 BiometricContext.getInstance(context));
     }
